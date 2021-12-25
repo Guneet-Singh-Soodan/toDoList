1a805a2b0e163899dc2f1f25f136463049f67ce0
Guneet-Singh-Soodan <gssoodan3281@gmail.com>
2021-12-25 16:10:50 +0530

    Initail commit

---
+++ date.js
@@ -0,0 +1,9 @@
+exports.getDay=function() {
+  var today=new Date();
+  var options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  };
+  return today.toLocaleDateString("en-US",options);
+}
